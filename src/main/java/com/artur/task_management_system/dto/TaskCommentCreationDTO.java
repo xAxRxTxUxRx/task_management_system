716a5ff6c8766c3c5ee9cb66873439b13c7babcf
xAxRxTxUxRx <khramchenkov.ciber@gmail.com>
2024-08-29 21:48:14 +0300

    added swagger docs for DTO classes

--- src/main/java/com/artur/task_management_system/dto/TaskCommentCreationDTO.java
+++ src/main/java/com/artur/task_management_system/dto/TaskCommentCreationDTO.java
@@ -1,5 +1,8 @@
 package com.artur.task_management_system.dto;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -8,7 +11,14 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Task Comment Creation DTO",
+                description = "Data Transfer Object for Task Comment creation"
+        )
+)
 public class TaskCommentCreationDTO {
+    @Schema(description = "A text value for comment creation")
     @NotBlank(message = "Text is mandatory")
     private String text;
 }
--- src/main/java/com/artur/task_management_system/dto/TaskViewDTO.java
+++ src/main/java/com/artur/task_management_system/dto/TaskViewDTO.java
@@ -4,6 +4,9 @@ import com.artur.task_management_system.model.TaskComment;
 import com.artur.task_management_system.model.User;
 import com.artur.task_management_system.model.attributes.TaskPriority;
 import com.artur.task_management_system.model.attributes.TaskStatus;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,14 +17,39 @@ import java.util.Set;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Task View DTO",
+                description = "Data Transfer Object for Task view"
+        )
+)
 public class TaskViewDTO {
+    @Schema(description = "The title of the task")
     private String title;
+
+    @Schema(description = "A brief description of the task")
     private String description;
+
+    @Schema(description = "The current status of the task",
+            allowableValues = {"IN_PROGRESS", "NEW", "COMPLETED"})
     private TaskStatus status;
+
+    @Schema(description = "The priority level of the task",
+            allowableValues = {"LOW", "MEDIUM", "HIGH"})
     private TaskPriority priority;
+
+    @Schema(description = "The date when the task was created")
     private LocalDateTime creationDate;
+
+    @Schema(description = "The deadline for completing the task")
     private LocalDateTime deadLineDate;
+
+    @Schema(description = "The user who created this task")
     private User author;
+
+    @Schema(description = "Set of users assigned to perform this task")
     private Set<User> performers;
+
+    @Schema(description = "Set of comments related to this task")
     private Set<TaskComment> comments;
 }
